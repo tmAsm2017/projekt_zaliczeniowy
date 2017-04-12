58d194121f9a61c71ed3d23c628857aba535bc94
Lukasz K <kruszeclukasz@gmail.com>
2017-04-12 02:05:45 +0200

    Add gps marker information

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,26 @@
 /**
  * Created by tomasz.maj on 09.04.2017.
  */
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {    
+    let gpsButton = document.getElementById("gps");
+    gpsButton.addEventListener("click", getPostition);
+}
+
+function getPostition(){
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+var onSuccess = function(position) {
+    document.getElementById("gps_info").innerHTML=
+        '<b>Latitude: </b>'          + position.coords.latitude + '<br>' +
+        '<b>Longitude:</b> '         + position.coords.longitude ;
+};
+
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+ 
